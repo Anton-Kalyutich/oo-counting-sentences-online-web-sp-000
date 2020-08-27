f0d4240c817e4b40fab59f407a983aa57131987d
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-08-27 00:03:52 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,6 @@ class String
   end
 
   def count_sentences
-    self.split(/[.!?]/).select {|string| !string.empty?}.count
+    self.split(/[.!?]/).select{|string| !string.empty?}.count
   end
 end
